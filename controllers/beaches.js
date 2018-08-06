4ba86f458d7e299eaf400e31d46ad35ae92905b7
Cleyra Uzcategui <cleyra.uzcategui@gmail.com>
2018-08-06 16:05:07 +0100

    Added CRUD actions and tested in Insomnia

---
+++ controllers/beaches.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const beachRouter = new express.Router();
+
+let beaches = ["Fidden Bay,", "Assynt", "Morar", "Sanna Sands"];
+
+
+//index
+beachRouter.get('/:id', function(req, res){
+  let index = req.params.id;
+  res.json(beaches[index]);
+});
+
+//show
+beachRouter.get('/', function(req, res){
+  res.json(beaches);
+});
+
+//create
+beachRouter.post('/', function(req, res){
+  beaches.push(req.body.beach);
+  res.json(beaches);
+});
+
+//update
+beachRouter.put('/:id', function(req, res){
+  let index = req.params.id;
+  beaches[index] = req.body.beach;
+  res.json(beaches);
+});
+
+//delete
+beachRouter.delete('/:id', function(req, res){
+  let index = req.params.id;
+  beaches.splice(index, 1);
+  res.json(beaches);
+});
+
+module.exports = beachRouter;
